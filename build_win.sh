392621b81b86c15cae35820284b8e67e1daee5e3
Elliot Saba <staticfloat@gmail.com>
2013-10-13 22:34:48 -0700

    Some further updates to the build_win.sh script
    Builds both 32 and 64 bit, but doesn't work yet due to https://github.com/JuliaLang/julia/issues/4514

--- build_win.sh
+++ build_win.sh
@@ -4,11 +4,11 @@ set -x
 
 # This script invoked by a cron job every X hours
 # This script functions best when the following are installed:
-#   git, python (with the "requests" module installed, for Travis-CI API), playtpus (must be in path)
+#   git, python (with the "requests" module installed, for Travis-CI API), wine, wget
 #
-# You must install Winston from source for proper bundling. This script assumes you have copied the
-#  contents of the relevant .julia/ directory to the directory pointed to by $JULIA_PKGDIR below.
-
+# Note that mingw-w64-dgn must be installed per these instructions:
+#   https://github.com/JuliaLang/julia/blob/master/README.windows.md
+# This script assumes both the 32 and 64-bit toolchains are installed to ~/cross-win{32,64}
 
 JULIA_GIT_BRANCH="master"
 if [[ ! -z "$1" ]]; then
@@ -17,7 +17,7 @@ fi
 
 # define variables
 JULIA_GIT_URL="https://github.com/JuliaLang/julia.git"
-BUILD_DIR=$(echo ~)/tmp/julia-packaging/win
+BUILD_DIR=$(echo ~)/tmp/julia-packaging/
 
 # cd to the location of this script
 cd $(dirname $0)
@@ -28,34 +28,47 @@ if [[ -d .git ]]; then
         git pull
 fi
 
-# Store everything in a temp dir
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR
+# We make 32 and 64-bit builds
+for ARCH in win32 win64; do
+	mkdir -p $BUILD_DIR/$ARCH
+	cd $BUILD_DIR/$ARCH
 
-# Checkout julia
-if test ! -d julia-${JULIA_GIT_BRANCH}; then
-        git clone ${JULIA_GIT_URL} julia-${JULIA_GIT_BRANCH}
-fi
+	# Checkout julia
+	if test ! -d julia-${JULIA_GIT_BRANCH}; then
+        	git clone ${JULIA_GIT_URL} julia-${JULIA_GIT_BRANCH}
+	fi
 
-# Go into our checkout of JULIA_GIT_URL
-cd julia-${JULIA_GIT_BRANCH}
-rm -rf deps/libuv deps/Rmath # This is the most common failure mode
-git submodule update
-git reset --hard
-git checkout ${JULIA_GIT_BRANCH}
-git fetch
-git reset --hard origin/${JULIA_GIT_BRANCH}
+	# Go into our checkout of JULIA_GIT_URL
+	cd julia-${JULIA_GIT_BRANCH}
+	rm -rf deps/libuv deps/Rmath # This is the most common failure mode
+	git submodule update
+	git reset --hard
+	git checkout ${JULIA_GIT_BRANCH}
+	git fetch
+	git reset --hard origin/${JULIA_GIT_BRANCH}
 
-# Find the last commit that passed a Travis build
-LAST_GOOD_COMMIT=$(${ORIG_DIR}/get_last_good_commit.py)
-if [ -z "$LAST_GOOD_COMMIT" ]; then
-        echo "ERROR: No good commits detected, going with HEAD!"
-        LAST_GOOD_COMMIT="HEAD"
-fi
+	# Find the last commit that passed a Travis build
+	LAST_GOOD_COMMIT=$(${ORIG_DIR}/get_last_good_commit.py)
+	if [ -z "$LAST_GOOD_COMMIT" ]; then
+        	echo "ERROR: No good commits detected, going with HEAD!"
+	        LAST_GOOD_COMMIT="HEAD"
+	fi
 
-git checkout -B ${JULIA_GIT_BRANCH} $LAST_GOOD_COMMIT
-if [[ "$?" != 0 ]]; then
-        echo "Couldn't checkout last good commit, going with master/HEAD!"
-        git checkout master
-fi
+	git checkout -B ${JULIA_GIT_BRANCH} $LAST_GOOD_COMMIT
+	if [[ "$?" != 0 ]]; then
+        	echo "Couldn't checkout last good commit, going with master/HEAD!"
+        	git checkout master
+	fi
 
+	export PATH=$(echo ~)/cross-$ARCH/bin:$PATH
+	echo $PATH
+	makevars="DEFAULT_REPL=basic"
+	if [[ "$ARCH" == "win64" ]]; then
+		makevars="$makevars XC_HOST=x86_64-w64-mingw"
+	else
+		makevars="$makevars XC_HOST=i686-w64-mingw32"
+	fi
+
+	make $makevars
+	make $makevars dist
+done
