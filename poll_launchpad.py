f5b930b4b67f8266839d87bf1532e7881ddcd6b2
Elliot Saba <staticfloat@gmail.com>
2014-01-24 15:46:12 -0500

    Include URL when posting to status.julialang.org

--- poll_launchpad.py
+++ poll_launchpad.py
@@ -3,10 +3,12 @@
 from launchpadlib.launchpad import Launchpad
 import requests, json
 
+url = "https://launchpad.net/~staticfloat/+archive/julianightlies"
+
 lp = Launchpad.login_anonymously('ppastats', 'production')
 archive = lp.people['staticfloat'].getPPAByName(name = "julianightlies")
 
 binaries = archive.getPublishedBinaries(status='Published')
 timestr = min([z['date_published'] for z in binaries.entries])
 
-requests.post("http://status.julialang.org/put/nightly", data=json.dumps({'target':'Ubuntu', 'time':timestr}))
+requests.post("http://status.julialang.org/put/nightly", data=json.dumps({'target':'Ubuntu', 'time':timestr, 'url':url}))
