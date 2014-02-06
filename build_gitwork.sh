4a9aaff39343ae999a9e5fa3b6f6aa783ea73cc8
Elliot Saba <staticfloat@gmail.com>
2014-02-05 16:32:23 -0800

    asd

--- build_gitwork.sh
+++ build_gitwork.sh
@@ -49,4 +49,4 @@ set -e
 JULIA_VERSION=$(cat VERSION)
 VERSDIR=$(cut -d. -f1-2 < VERSION)
 BANNER="Official http://julialang.org/ release"
-makevars="VERBOSE=1 TAGGED_RELEASE_BANNER=$BANNER"
+makevars="VERBOSE=1 TAGGED_RELEASE_BANNER=\"$BANNER\""
